7d02ed7dd80121e835725f6044c92bf9fe901ebe
RSEANCAS <ronald.jsb@hotmail.com>
2020-07-11 12:48:19 -0500

    Reto 1 y Reto 2

---
+++ README.md
@@ -0,0 +1,16 @@
+# ¿Cuáles son las responsabilidades de un manejador de rutas?
+
+* Definir la navegacion de nuestra aplicación y a qué vistas serán de acceso público y que vistas tendrán restricciones.
+
+
+# ¿Para qué sirve un manejador de rutas?
+
+* Un manejador de rutas nos permite asignar una url a un componente para renderizar.
+
+
+# Describir el proceso, que se realiza en el ambito de front para logearse en un sistema.
+
+* El usuario debe ingresar las credenciales (normalmente usuario y contraseña.)
+* La aplicación web deberá enviar la información al servidor para que sea validada.
+* Si las credenciales ingresadas son válidas la aplicación web te permitirá acceder al contenido restringido de la aplicación web.
+* En caso contrario, se mostrará un mensaje indicadando que las credenciales no són válidas.
---
+++ src/components/Header.vue
@@ -0,0 +1,56 @@
+<template>
+    <header class="header">
+        <div class="main">
+            <a href="/" class="logo" v-text="titulo"></a>
+            <a href="#" v-text="boton.agregar" @click.prevent="btnAgregarClick"></a>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'header-component',
+    data: () => {
+        return {
+            titulo: 'VIDEO PACHA TV',
+            boton: {
+                agregar: 'AGREGAR'
+            }
+        }
+    },
+    methods: {
+        btnAgregarClick() {
+            // console.log(this);
+            this.$parent.open = true;
+            this.$parent.item = null;
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+#app > div {
+    &>.header {
+    display: flex;
+    background-color: #FF5252;
+    padding: 10px 40px;
+        & .main {
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+            align-items: center;
+        }
+
+        & a {
+            color: white;
+            text-shadow: black 0px 3px 3px;
+            font-size: 13px;
+        }
+
+        & a.logo {
+            font-size: 20px;
+            font-weight: bold;
+        }
+    }
+}
+</style>
